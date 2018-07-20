a0fdf519fb941486949b0376901a1b567cfe2e12
segasi <segasi@gmail.com>
2018-07-20 15:56:41 -0500

    Paquetes

---
+++ 02_codigo/importar_analizar_datos.R
@@ -0,0 +1,10 @@
+### Paquetes ----
+library(pacman)
+p_load(animation, cowplot, curl, extrafont, 
+       forcats, gganimate, ggforce, ggmap, 
+       ggraph, ggrepel, ggridges, janitor, 
+       lubridate, mapdata, maps, maptools, 
+       purrr, readxl, rgdal, rgeos, scales, sf,
+       sp, stringi, stringr, stringdist, tweenr, 
+       tidyr, tidygraph, tidyverse, treemapify, zoo)
+
