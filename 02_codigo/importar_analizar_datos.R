5e45b67346f4aa997c23a327423cfdd6da050754
segasi <segasi@gmail.com>
2018-07-20 15:56:57 -0500

    Setup general

--- 02_codigo/importar_analizar_datos.R
+++ 02_codigo/importar_analizar_datos.R
@@ -8,3 +8,7 @@ p_load(animation, cowplot, curl, extrafont,
        sp, stringi, stringr, stringdist, tweenr, 
        tidyr, tidygraph, tidyverse, treemapify, zoo)
 
+### Setup general ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") 
+options(scipen = 9999)
+
